d42f318906d6e7909c85eb4c49483b32bf1183b5
Hafiq <hafiqiqmal93@gmail.com>
2017-05-29 15:40:42 +0800

    fix permission listener

--- app/src/main/java/com/java/mvp/mvpandroid/permission/PermissionConnector.java
+++ app/src/main/java/com/java/mvp/mvpandroid/permission/PermissionConnector.java
@@ -1,18 +1,9 @@
 package com.java.mvp.mvpandroid.permission;
 
-import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.DexterError;
-import com.karumi.dexter.listener.PermissionRequest;
-
 /**
  * @author : hafiq on 05/03/2017.
  */
 
 public interface PermissionConnector {
-
-    void showPermissionGranted(String permissionName);
-    void isAllPermissionGranted(boolean isAllGranted);
-    void showPermissionDenied(String permission, boolean isPermanentlyDenied);
-    void showPermissionRationale(PermissionRequest permissions, PermissionToken token);
-    void showPermissionError(DexterError error);
+    void isPermissionGranted(boolean granted);
 }
